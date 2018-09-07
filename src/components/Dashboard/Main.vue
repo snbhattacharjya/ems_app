a37bf931a417b62f7a0bb37c5f64bad9f1ce1256
Saikat Narayan Bhattacharjya <snbhattacharjya@gmail.com>
2018-09-07 17:45:02 +0530

    Main Dashboard

--- src/components/Dashboard/Main.vue
+++ src/components/Dashboard/Main.vue
@@ -10,15 +10,15 @@ import district from '@/components/Dashboard/District'
 import office from '@/components/Dashboard/Office'
 
 export default {
-  name: 'OfficeDashboard',
+  name: 'Dashboard',
   data () {
     return {
       user_level: 0
     }
   },
   computed: {
-    getUserLevel: () => {
-      return this.$store.getters.getUserLevel
+    getUserLevel() {
+       return this.$store.getters.getUserLevel
     }
   },
   components: {
